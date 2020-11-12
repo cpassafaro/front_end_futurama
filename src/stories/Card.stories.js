04d4333c236f02c67bc130820fda418e94e1dc12
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2020-11-12 16:53:34 -0500

    card componet

---
+++ src/stories/Card.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+// stortiesOf('Card',module)
+// .add("Card", () => <Card
+//     type='card'
+// />)
+
+export default {
+    title: 'Card'
+}

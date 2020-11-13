d9879257f3a546f37c3255e3c79b05551ba0de48
lina mierkE <lina_maria89@hotmail.com>
2020-11-13 17:58:54 -0500

    edit age

--- src/stories/Card.js
+++ src/stories/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState}from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,6 +8,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import TextField from '@material-ui/core/TextField';
+
 
 import "./card.css";
 
@@ -23,6 +25,33 @@ const useStyles = makeStyles({
 export default function MediaCard(props) {
   const classes = useStyles();
   // console.log(props.characters.data)
+
+  const [newAge, setNewAge] = useState("")
+
+
+  function handleEdit(age){
+    console.log(newAge)
+    fetch("https://futurama-project.herokuapp.com/characters/" + age ,{
+
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+
+            status: newAge
+        })
+
+    })
+        .then(res => res.text())
+        .then(res => console.log(res))
+   
+
+}
+
+
+function handleChange(e){
+    setNewAge(e.target.value)
+
+}
 
   return (
 
@@ -65,8 +94,9 @@ export default function MediaCard(props) {
                   </CardActionArea>
 
                   <CardActions>
-                    <Button size="small" color="primary">
-                      Edit Character
+                  <TextField placeholder={card.age}  onChange={handleChange} />
+                    <Button onClick= {()=> handleEdit(card.age)} size="small" color="primary">
+                      Edit Age
 
                     </Button>
                     <Button size="small" color="primary">
